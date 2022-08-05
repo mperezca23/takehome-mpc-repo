23b797b54a8f8eb96d79abb5492e7fad6348d299
mperezca23 <68745736+mperezca23@users.noreply.github.com>
2022-08-05 13:15:23 -0400

    test5

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,6 @@ import pandas as pd
 
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3, 4]}
-    df = pd.Dataframe(data=d)
+    df = pd.DataFrame(data=d)
     print(df)
     print("Done x1.3")
